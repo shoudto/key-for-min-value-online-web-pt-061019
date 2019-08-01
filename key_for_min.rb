cc65b004343941af3c6ac0a3552cad0d22890066
Oscar Shoudt <shoudto@gmail.com>
2019-08-01 16:24:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@ require 'pry'
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-# name_hash = :blake => 500, :ashley => 2, :adam => 1
+# hash = :blake => 500, :ashley => 2, :adam => 1
 
 def key_for_min_value(hash)
   min_key = nil
